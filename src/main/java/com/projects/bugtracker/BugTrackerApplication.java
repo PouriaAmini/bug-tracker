8a0db004c22aa3e92f513eac02ef0eaf5a3aa342
Pouria Amini <aminipooria1@gmail.com>
2021-12-19 14:45:05 -0500

    initial commit

---
+++ src/main/java/com/projects/bugtracker/BugTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.projects.bugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/bugtracker/BugTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.bugtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
