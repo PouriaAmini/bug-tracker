6666daa7c232444fb3e0b1e4b4c01624b0a05e55
Pouria Amini <aminipooria1@gmail.com>
2021-12-19 21:00:08 -0500

    Removed User Authentication

--- src/main/java/com/projects/bugtracker/model/UserAccount.java
+++ src/main/java/com/projects/bugtracker/model/UserAccount.java
@@ -3,18 +3,13 @@ package com.projects.bugtracker.model;
 import com.projects.bugtracker.model.enumeration.UserRole;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.Collection;
-import java.util.Collections;
 
 @Data
 @Entity
 @NoArgsConstructor
-public class UserAccount implements UserDetails {
+public class UserAccount {
 
     @Id
     @SequenceGenerator(
@@ -43,55 +38,4 @@ public class UserAccount implements UserDetails {
     private String password;
     private Boolean locked;
     private Boolean enabled;
-
-    public UserAccount(User user,
-                       String email,
-                       String password,
-                       UserRole position,
-                       Boolean locked,
-                       Boolean enabled
-    ) {
-        this.user = user;
-        this.email = email;
-        this.password = password;
-        this.position = position;
-        this.locked = locked;
-        this.enabled = enabled;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(position.name());
-        return Collections.singletonList(authority);
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return !locked;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return enabled;
-    }
 }
