3ebe2c9b850da8af98a5028ee00d3db81d9f6491
Pouria Amini <aminipooria1@gmail.com>
2021-12-22 17:04:53 -0500

    Add enumUpdate for Priority and Status

--- src/main/java/com/projects/bugtracker/service/GroupService.java
+++ src/main/java/com/projects/bugtracker/service/GroupService.java
@@ -3,6 +3,8 @@ package com.projects.bugtracker.service;
 import com.projects.bugtracker.model.Bug;
 import com.projects.bugtracker.model.Group;
 import com.projects.bugtracker.model.User;
+import com.projects.bugtracker.model.enumeration.Priority;
+import com.projects.bugtracker.model.enumeration.Status;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,11 +13,16 @@ import java.util.UUID;
 public interface GroupService {
     Optional<Group> get(UUID id);
     Optional<Group> create(Group group, UUID projectId);
+    Boolean delete(UUID id);
+    List<Group> searchGroup(String name);
+    void updateEnum(
+            Group group,
+            Status newStatus,
+            Priority newPriority
+    );
     Optional<Group> update(
             Group group,
             User newContributor,
             Bug newBug
     );
-    Boolean delete(UUID id);
-    List<Group> searchGroup(String name);
 }
--- src/main/java/com/projects/bugtracker/service/ProjectService.java
+++ src/main/java/com/projects/bugtracker/service/ProjectService.java
@@ -3,6 +3,8 @@ package com.projects.bugtracker.service;
 import com.projects.bugtracker.model.Group;
 import com.projects.bugtracker.model.Project;
 import com.projects.bugtracker.model.User;
+import com.projects.bugtracker.model.enumeration.Priority;
+import com.projects.bugtracker.model.enumeration.Status;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,11 +13,16 @@ import java.util.UUID;
 public interface ProjectService {
     Optional<Project> get(UUID id);
     Project create(Project project);
+    Boolean delete(UUID id);
+    List<Project> searchProject(String name);
+    void updateEnum(
+            Project project,
+            Status newStatus,
+            Priority newPriority
+    );
     Optional<Project> update(
             Project project,
             User newContributor,
             Group newGroup
     );
-    Boolean delete(UUID id);
-    List<Project> searchProject(String name);
 }
