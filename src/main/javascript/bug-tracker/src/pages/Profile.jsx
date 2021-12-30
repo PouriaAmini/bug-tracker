61d986f8e3747c04e4928d6010d76a10d3602c11
Pouria Amini <aminipooria1@gmail.com>
2021-12-29 20:47:07 -0500

    Create new components for assigned bugs, profile and settings

---
+++ src/main/javascript/bug-tracker/src/pages/Profile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const user = JSON.parse(localStorage.getItem("user")).Users[0]
+
+const Profile = () => {
+    return (
+        <div>
+            <h2 className="page-header">
+                {
+                    `${user.firstName} ${user.lastName}`
+                }
+            </h2>
+            <div className="row">
+                <div className="col-12">
+                    <div className="card">
+                        <div className="card__body">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
