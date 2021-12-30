bcac40099533d11e975f4b1e6aa995afae86aa26
Pouria Amini <aminipooria1@gmail.com>
2021-12-29 20:50:43 -0500

    resolve search lag

--- src/main/javascript/bug-tracker/src/services/SearchAction.js
+++ src/main/javascript/bug-tracker/src/services/SearchAction.js
@@ -1,56 +1,75 @@
 
 export async function SearchAction(url, token, searchResult) {
 
-    await fetch(`http://localhost:8080/api/user/search/${url}`,
-        {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
-        .then(response => {
-            response.json().then(data => {
-                searchResult.push(...data.data["Users"])
-            })}
-        );
+    function getUser() {
 
-    await fetch(`http://localhost:8080/api/project/search/${url}`,
-        {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
-        .then(response => {
-            response.json().then(data => {
-                searchResult.push(...data.data["Projects"])
-            })}
-        );
+        return fetch(`http://localhost:8080/api/user/search/${url}`,
+            {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then(response => (
+                response.json().then(data => (
+                    searchResult.push(...data.data["Users"])
+                )))
+            );
+    }
 
-    await fetch(`http://localhost:8080/api/group/search/${url}`,
-        {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
-        .then(response => {
-            response.json().then(data => {
-                searchResult.push(...data.data["Groups"])
-            })}
-        );
+    function getProject() {
 
-    await fetch(`http://localhost:8080/api/bug/search/${url}`,
-        {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
-        .then(response => {
-            response.json().then(data => {
-                searchResult.push(...data.data["Bugs"])
-            })}
-        );
-    return searchResult;
+        return fetch(`http://localhost:8080/api/project/search/${url}`,
+            {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then(response => (
+                response.json().then(data => (
+                    searchResult.push(...data.data["Projects"])
+                )))
+            );
+    }
+
+    function getGroup() {
+
+        return fetch(`http://localhost:8080/api/group/search/${url}`,
+            {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then(response => (
+                response.json().then(data => (
+                    searchResult.push(...data.data["Groups"])
+                )))
+            );
+    }
+
+    function getBug() {
+
+        return fetch(`http://localhost:8080/api/bug/search/${url}`,
+            {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then(response => (
+                response.json().then(data => {
+                    searchResult.push(...data.data["Bugs"])
+                }))
+            );
+    }
+
+    function getSearchResult() {
+        return Promise.all([getBug(), getGroup(), getProject(), getUser()])
+    }
+
+    return getSearchResult().then(response => {
+        return searchResult;
+    });
 }
